45e5e45362575d9417a8cce99b9ddf71c786b579
Golubkov <golubkov2506@gmail.com>
2022-08-04 21:23:12 +0300

    new

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,169 +1,256 @@
-# Указываем, что мы хотим разворачивать окружение в AWS
-provider "aws" {
-  region = "us-east-1"
-}
-
-
-# Узнаём, какие есть Дата центры в выбранном регионе
-data "aws_availability_zones" "available" {}
-
-# Ищем образ с последней версией Ubuntu
-data "aws_ami" "ubuntu" {
-  owners      = ["099720109477"]
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-}
-
-# Созаём правило, которое будет разрешать трафик к нашим серверам
-resource "aws_security_group" "web" {
-  name = "Dynamic Security Group"
-
-  dynamic "ingress" {
-    # Зададим правило, по каким портам можно обращаться к нашим серверам
-    for_each = ["22", "80"]
-    content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
     }
   }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  required_version = ">= 0.13"
   }
 
-  tags = {
-    Name  = "Web access for Application"
+  provider "yandex" {
+  token     = "AQAAAAAGr2zaAATuwUg_9EhAK0mPh1LjHE9LSVk"
+  cloud_id  = "b1g2593k11ai4j2fpude"
+  folder_id = "b1gnlcb1hiv9e03692tv"
+  zone      = "ru-central1-a"
   }
-}
 
-
-# Создаём Launch Configuration - это сущность, которая определяет конфигурацию запускаемых серверов. Размер, , 
-
-resource "aws_launch_configuration" "web" {
-  name_prefix     = "NGINX-"
-  # какой будет использоваться образ
-  image_id        = data.aws_ami.ubuntu.id
-  # Размер машины (CPU и память)
-  instance_type   = "t2.micro"
-  # какие права доступа
-  security_groups = [aws_security_group.web.id]
-  # какие следует запустить скрипты при создании сервера
-  user_data       = file("user_data.sh")
-  # какой SSH ключ будет использоваться 
-  key_name = "id_rsa"
-  # Если мы решим обновить инстанс, то, прежде, чем удалится старый инстанс, который больше не нужен, должен запуститься новый
-  lifecycle {
-    create_before_destroy = true
+  resource "yandex_vpc_network" "network-1" {
+  name = "network-1"
   }
-}
 
+  resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  }
 
-# AWS Autoscaling Group для указания, сколько нам понадобится инстансов 
-resource "aws_autoscaling_group" "web" {
-  name                 = "ASG-${aws_launch_configuration.web.name}"
-  launch_configuration = aws_launch_configuration.web.name
-  min_size             = 2
-  max_size             = 2
-  min_elb_capacity     = 2
-  health_check_type    = "ELB"
-  # и в каких подсетях, каких Дата центрах их следует разместить
-  vpc_zone_identifier  = [aws_default_subnet.availability_zone_1.id, aws_default_subnet.availability_zone_2.id]
-  # Ссылка на балансировщик нагрузки, который следует использовать 
-  load_balancers       = [aws_elb.web.name]
+  resource "yandex_compute_instance" "vm-1" {
+  name = "test"
+  platform_id = "standard-v1"
+  zone = "ru-central1-a"
 
-  dynamic "tag" {
-    for_each = {
-      Name   = "Nginx Server in Auto Scalling Group"
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = "${file("~/.ssh/id_rsa")}"
+    host = "${yandex_compute_instance.vm-1.network_interface.0.nat_ip_address}"
+    agent = false
+  }
+
+  resources {
+    cores = 2
+    memory = 2
+  }
+
+  provisioner "file" {
+    source = "./user_data.sh"
+    destination = "/home/ubuntu/user_data.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x ~/user_data.sh",
+      "sudo ~/user_data.sh",
+    ]
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd87tirk5i8vitv9uuo1"
     }
-    content {
-      key                 = tag.key
-      value               = tag.value
-      propagate_at_launch = true
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+  resource "yandex_compute_instance" "vm-2" {
+  name = "test-2"
+  platform_id = "standard-v1"
+  zone = "ru-central1-a"
+
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = "${file("~/.ssh/id_rsa")}"
+    host = "${yandex_compute_instance.vm-2.network_interface.0.nat_ip_address}"
+    agent = false
+  }
+
+  resources {
+    cores = 2
+    memory = 2
+  }
+
+  provisioner "file" {
+    source = "./user_data.sh"
+    destination = "/home/ubuntu/user_data.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x ~/user_data.sh",
+      "sudo ~/user_data.sh",
+    ]
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd87tirk5i8vitv9uuo1"
     }
   }
-  
-  lifecycle {
-    create_before_destroy = true
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+  resource "yandex_compute_instance" "vm-3" {
+  name = "haproxy"
+  platform_id = "standard-v1"
+  zone = "ru-central1-a"
+
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = "${file("~/.ssh/id_rsa")}"
+    host = "${yandex_compute_instance.vm-3.network_interface.0.nat_ip_address}"
+    agent = false
+  }
+
+  resources {
+    cores = 2
+    memory = 2
+  }
+
+  # provisioner "file" {
+  #   source = "./user_data.sh"
+  #   destination = "/home/ubuntu/user_data.sh"
+  # }
+
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "sudo chmod +x ~/user_data.sh",
+  #     "sudo ~/user_data.sh",
+  #   ]
+  # }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd87tirk5i8vitv9uuo1"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
 }
 
-# Elastic Load Balancer проксирует трафик на наши сервера 
-resource "aws_elb" "web" {
-  name               = "Nginx-Highly-Available-ELB"
-  # перенаправляет трафик на несколько Дата центров
-  availability_zones = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
-  security_groups    = [aws_security_group.web.id]
-  # слушает на порту 80
-  listener {
-    lb_port           = 80
-    lb_protocol       = "http"
-    instance_port     = 80
-    instance_protocol = "http"
-  }
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 10
-    timeout             = 60
-    target              = "HTTP:80/"
-    interval            = 70
-  }
-  tags = {
-    Name = "Nginx-Highly-Available-ELB"
-  }
-}
+ resource "yandex_compute_instance" "vm-4" {
+  name = "reactjs"
+  platform_id = "standard-v1"
+  zone = "ru-central1-a"
 
-# Созаём подсети в разных Дата центрах
-resource "aws_default_subnet" "availability_zone_1" {
-  availability_zone = data.aws_availability_zones.available.names[0]
-}
-
-resource "aws_default_subnet" "availability_zone_2" {
-  availability_zone = data.aws_availability_zones.available.names[1]
-}
-
-resource "aws_eip" "my_static_ip" {
-  instance = aws_instance.my_webserver.id
-  tags = {
-    Name  = "ReactJS Server IP for Ansible"
-  }
-}
-
-
-# Запускаем инстанс
-resource "aws_instance" "my_webserver" {
-  # с выбранным образом 
-  ami                    = data.aws_ami.ubuntu.id
-  # и размером (количество ЦПУ и памяти зависит от этой директивы) 
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web.id]
-  key_name = "id_rsa"
-  tags = {
-    Name  = "ReactJS Server"
-    Env = "Production"
-    Tier = "Backend"
-    CM = "Ansible"
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = "${file("~/.ssh/id_rsa")}"
+    host = "${yandex_compute_instance.vm-4.network_interface.0.nat_ip_address}"
+    agent = false
   }
 
-  lifecycle {
-    create_before_destroy = true
+  resources {
+    cores = 2
+    memory = 2
   }
 
+  # provisioner "file" {
+  #   source = "./user_data.sh"
+  #   destination = "/home/ubuntu/user_data.sh"
+  # }
+
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "sudo chmod +x ~/user_data.sh",
+  #     "sudo ~/user_data.sh",
+  #   ]
+  # }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd87tirk5i8vitv9uuo1"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
 
-# Выведем в консоль DNS имя нашего сервера 
-output "web_loadbalancer_url" {
-  value = aws_elb.web.dns_name
-}
+  output "external_ip_address_reactjs" {
+  description = "My external IP"
+  value = yandex_compute_instance.vm-4.network_interface.0.nat_ip_address
+  }
 
-# Выведем IP адрес сервера
-output "my_web_site_ip" {
-  description = "Elatic IP address assigned to our ReactJS Server"
-  value       = aws_eip.my_static_ip.public_ip
-}
+
+  output "internal_ip_address_" {
+  description = "My external IP"
+  value = yandex_compute_instance.vm-4.network_interface.0.ip_address
+  }
+
+
+
+  output "internal_instance_ip" {
+  description = "My internal IP"
+  value = [yandex_compute_instance.vm-1.network_interface.0.ip_address
+  ,yandex_compute_instance.vm-2.network_interface.0.ip_address]
+  }
+
+
+  output "external_ip_address_vm_1" {
+  description = "My external IP"
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+  }
+
+  output "internal_ip_address_vm_2" {
+  description = "My internal IP"
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+  }
+
+
+  output "external_ip_address_vm_2" {
+  description = "My external IP"
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+  }
+
+  output "internal_ip_address_haproxy" {
+  description = "My internal IP"
+  value = yandex_compute_instance.vm-3.network_interface.0.ip_address
+  }
+
+
+  output "external_ip_address_haproxy" {
+  description = "My external IP"
+  value = yandex_compute_instance.vm-3.network_interface.0.nat_ip_address
+  }
